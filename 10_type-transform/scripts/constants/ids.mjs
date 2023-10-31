67eab1c3adb81f4b091072d8de41e3c04fb8557d
hellsgor <hellsgor@gmail.com>
2023-11-01 01:47:01 +0400

    вёрстка формы добавления студента
    (cherry picked from commit 790485bca596d218d30d08b28cf877cd7edf6d8e)

---
+++ 10_type-transform/scripts/constants/ids.mjs
@@ -0,0 +1,12 @@
+export const ids = {
+  STUDENTS_TABLE_BODY_ID: 'table-body',
+  ADD_STUDENT_BUTTON_ID: 'add-student-button',
+
+  // new student form
+  NEW_STUDENT_FORM: 'new-student-form',
+  NEW_STUDENT_SURNAME: 'new-student-surname',
+  NEW_STUDENT_NAME: 'new-student-name',
+  NEW_STUDENT_PATRONYMIC: 'new-student-patronymic',
+  NEW_STUDENT_DOB: 'new-student-dob',
+  NEW_STUDENT_FACULTY: 'new-student-faculty',
+}
