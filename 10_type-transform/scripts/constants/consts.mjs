6b95ee08d97b0e64ec62c81fca76212f71b4220c
hellsgor <hellsgor@gmail.com>
2023-11-08 00:57:42 +0400

    добавлены id колонок таблицы
    (cherry picked from commit 932a746887577af1672ca2ca7a5fbabfe79b6848)

--- 10_type-transform/scripts/constants/consts.mjs
+++ 10_type-transform/scripts/constants/consts.mjs
@@ -5,6 +5,13 @@ export const constants = {
   MIN_DIFFERENCE_BETWEEN_DOB_YEAR_AND_YEAR_OF_STUDY_START: 16,
 }
 
+export const tableColumnsIDs = {
+  fio: 'students-fio-column',
+  faculty: 'students-faculty-column',
+  dob: 'students-dob-column',
+  study: 'students-years-of-study'
+}
+
 export const regExps = {
   SURNAME: /^([А-ЯЁ]{1}[а-яё]{1,24}-[А-ЯЁ]{1}[а-яё]{1,23}|[А-ЯЁ]{1}[а-яё]{1,49})$/,
   NAME_OR_PATRONYMIC: /^[А-ЯЁ]{1}[а-яё]{1,49}$/,
