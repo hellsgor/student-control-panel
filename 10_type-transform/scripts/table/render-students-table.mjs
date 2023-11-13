5b4e0ac4560b3927bd545f86c12faa251703fa58
hellsgor <hellsgor@gmail.com>
2023-11-13 23:21:21 +0400

    оптимизирована renderStudentsTable()
    (cherry picked from commit fa8f36c59ba2f410b31f472beabc199520304541)

--- 10_type-transform/scripts/table/render-students-table.mjs
+++ 10_type-transform/scripts/table/render-students-table.mjs
@@ -4,11 +4,13 @@ import {commonIDs} from "../constants/ids.mjs";
 export function renderStudentsTable(studentsArray) {
   const tableBody = document.getElementById(commonIDs.STUDENTS_TABLE_BODY_ID);
   tableBody.innerHTML = '';
-  studentsArray.forEach((student) => {
-    const studentRow = getStudentItem(student);
-    if (!studentRow) {
-      return;
-    }
-    tableBody.appendChild(studentRow);
-  })
+  if (studentsArray.length > 0) {
+    studentsArray.forEach((student) => {
+      const studentRow = getStudentItem(student);
+      if (!studentRow) {
+        return;
+      }
+      tableBody.appendChild(studentRow);
+    })
+  }
 }
