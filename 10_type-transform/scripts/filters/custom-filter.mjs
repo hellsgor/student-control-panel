e02c2ebafd1c159fbcee8820c60f9c2c927300ca
hellsgor <hellsgor@gmail.com>
2023-11-13 23:21:47 +0400

    добавлена фильтрация по году начала обучения
    (cherry picked from commit af433b8b7425ad06715aa13691b6ecac5a0ecec8)

--- 10_type-transform/scripts/filters/custom-filter.mjs
+++ 10_type-transform/scripts/filters/custom-filter.mjs
@@ -9,5 +9,9 @@ export function customFilter(students, control) {
   if (control.id === filtersIDs.filtersControls.FACULTY) {
     return students.filter((student) => student.faculty.includes(control.value));
   }
+  if (control.id === filtersIDs.filtersControls.STUDY_START) {
+    return students.filter((student) =>
+      student.startStudiesYear === Number(control.value))
+  }
   return students;
 }
